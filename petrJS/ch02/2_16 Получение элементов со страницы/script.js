5f95a8fa31fa0ada8d5ac27c471c636e44707638
Alex <BoSacha20@gmail.com>
2020-09-28 17:30:39 +0300

    jsPtrHW5

---
+++ petrJS/ch02/2_16 Получение элементов со страницы/script.js
@@ -0,0 +1,13 @@
+//<!--script.js-->
+let box = document.getElementById('box'),
+	btn = document.getElementsByTagName('button'),
+	circle = document.getElementsByClassName('circle'),
+	heart = document.guerySelectorAll('.heart'),
+	oneHeart = document.guerySelector('.heart');
+
+console.log(box); // вывод с id="box"
+console.log(btn); // вывод с tagName=button (это псевдомассив)
+console.log(btn[4]); // вывод с tagName=button1 (это элемент псевдомассива)
+console.log(circle[2]);// вывод элемента псевдоколлекции
+console.log(heart[1]); // самые пополярные методы
+console.log(oneHeart);
