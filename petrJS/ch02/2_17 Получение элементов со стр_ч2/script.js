df3aaf7e4611bb0c2e63c43c7738c3c333eb2604
Alex <BoSacha20@gmail.com>
2020-10-05 18:00:34 +0300

    jsPtrHW7

--- petrJS/ch02/2_17 Получение элементов со стр_ч2/script.js
+++ petrJS/ch02/2_17 Получение элементов со стр_ч2/script.js
@@ -6,7 +6,7 @@ let box = document.getElementById('box'),
 	heart = document.querySelectorAll('.heart'),
 	oneHeart = document.querySelector('.heart'),
 	myheart = document.querySelectorAll('.myheart'),
-	oneMyheart = document.querySelectorAll('.myheart')
+	oneMyheart = document.querySelectorAll('.myheart'),
 	wrapper = document.querySelector('.wrapper');
 
 // ***** изменение стилей ******
@@ -24,7 +24,7 @@ for (let i=0; i < heart.length; i++) {
 
 //for (let i=0; i < myheart.length; i++) {
 //	myheart[i].style.backgroundColor = 'yellow';
-//}
+//};
 
 myheart.forEach(function(item, i, hearts){
 	item.style.backgroundColor = 'yellow';
@@ -35,12 +35,36 @@ myheart.forEach(function(item, i, hearts){
 
 // ******* создание элемента ******
 let div = document.createElement('div'),
-	text = document.createTextNode('Тут был я');
+	text = document.createTextNode('Тут был я'),
+	myDiv = document.createElement('myDiv'),
+	myTegDiv = document.createElement('myTegDiv');
 
 //div.className. . . . устарел
 div.classList.add('black');
+myDiv.classList.add('myBlack');
+myTegDiv.classList.add('myTegBlack');
 
-document.body.appendChild(div);
+div.innerHTML = '<h3>"Это мой Div</h3>';
+myDiv.innerHTML = 'Текст myDiv';
+myTegDiv.innerHTML = text;
+// чтобы обезопасить занесение текста лучше пользоваться другим методам,
+// который позволит написать только текст
+div.textContent = '<h3>"Это тоже мой Div</h3>';
+// чтобы увидеть работу всех методов нужно запустить по шагам
+// ибо код выполняется сверху вниз поэтому результатом будет 
+// выполнение последнего метода(промежуточных действий не увидишь)
+
+wrapper.appendChild(div);// вставить в конец элемента wrapper
+document.body.insertBefore(div,circle[0]);
+
+document.body.removeChild(circle[1]);
+
+document.body.insertBefore(myTegDiv,heart[1]); // вставить в конец body
+
+document.body.replaceChild(btn[5],heart[2]);
+wrapper.replaceChild(btn[5], myheart[1]);
+
+wrapper.insertBefore(myDiv,myheart[0]);
 
 console.log(div);
 
