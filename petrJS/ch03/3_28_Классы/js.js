687975ee66e1a0431187cce80155ed6361b33a6b
Alex <BoSacha20@gmail.com>
2020-11-17 18:30:15 +0300

    jsPtrHWES6

--- petrJS/ch03/3_28_Классы/js.js
+++ petrJS/ch03/3_28_Классы/js.js
@@ -1,33 +1,54 @@
-// ES6 параметры по умолчанию js.js
-function calcOrDouble(number, basis) {
-    basis = basis || 2; //так странно делали до ES6 в ES5
-    console.log(number*basis);
-};
-calcOrDouble(7,9); //63
-calcOrDouble(5); //10 просто удвоилось
-
-// В ES6 параметры по умолчанию можно записывать прямо при объявлении функции
-
-function calcOrDoubleES6(numberES6 = 2, basisES6 = 2) {
-    console.log(numberES6*basisES6);
-};
-calcOrDoubleES6();
-calcOrDoubleES6(7,7);
-calcOrDoubleES6(5);
-calcOrDoubleES6(null,7);
-calcOrDoubleES6(undefined, 148);
-
-console.log('Вызов функции кнопкой');
-let btn = document.querySelector('button');
-
-btn.addEventListener('click', function(){
-    console.log(calcOrDoubleES6(45, 1005));//вывод 42225
-    //это можно использовать - при нажатии перекрасить кнопку/что-нибудь
-    this.style.backgroundColor = 'red';
-    // а вот функция в фунукции
-    function calcThis()
-    {
-        console.log(calcOrDoubleES6(700, 800));//вывод 560000
+// ES6 параметры Классы js.js
+// эксперименты Ptr
+class Rectangle {
+    constructor(height = 7, width = 4) {
+        this.height = height;
+        this.width = width;
+    }// ; не ставиться
+    calcArea() {
+        return this.height * this.width;
     }
-    calcThis();
-});
+}
+// теперь мы можем наплодить квадратиков сколько нам нужно
+// так можно поступить с чем угодно используя классы (прототипы)
+  const square = new Rectangle(12,12);
+  const squareMy = new Rectangle(15,15);
+  const squareMyWp = new Rectangle();
+  console.log("площадь квадратиков: - ", square.calcArea(), " мой квадратик -", squareMy.calcArea(), "с параметрами по умолчанию -", squareMyWp.calcArea());
+  // Инкапсуляция
+// Инкапсуляция есть ни что иное, как реализация приватности. В JavaScript подобная концепция реализуется благодаря функциям и их областям видимости.
+// Функция конструктор
+ const Person = function(name) {
+    // Приватная функция
+    let log = function(message) {
+        console.log(message);
+    };
+
+    // Публичное свойство
+    this.name = name;
+console.log(Person(name));
+    // Публичный метод
+    this.logger = function(message) {
+        log('Public ' + message);
+    };
+};
+
+var Person1 = (function() {
+    // Приватная функция
+    let log = function(message) {
+        console.log(message);
+    };
+
+    let Person2 = function(name) {
+        // Публичное свойство
+        this.name = name;
+    };
+
+    // Публичный метод
+    Person.prototype.logger = function(message) {
+        log('Public ' + message);
+    };
+
+    // Экспорт публичной функции
+    return Person1, Person2;
+})();
