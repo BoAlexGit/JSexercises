062f4c987ffd9857594d877d613fb93db0e8ae2a
Alex <BoSacha20@gmail.com>
2021-02-11 17:10:08 +0300

    jsPtrHW14_31

--- petrJS/ch04/32_Задание 14 Калькулятор промисы на  примерах/alex_testDomain/js/script.js
+++ petrJS/ch04/32_Задание 14 Калькулятор промисы на  примерах/alex_testDomain/js/script.js
@@ -1,5 +1,5 @@
-
-let inputRub = document.getElementById('rub'),
+/*простой конвертер валют /home/alex/jsGit/petrJS/ch04/32_Задание 14 Калькулятор промисы на  примерах/alex_testDomain/js/script.js*/
+let inputRub = document.getElementById('rub'),//п
     inputUsd = document.getElementById('usd');
 
 inputRub.addEventListener('input', () => {
@@ -8,9 +8,13 @@ inputRub.addEventListener('input', () => {
     request.open('GET', 'js/current.json');
     request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
     request.send();
+    // status
+    // statusText
+    // responseText / response
+    // readyState
     
     request.addEventListener('readystatechange', function() {
-        if (request.readyState === 4 && request.status == 200) {
+        if (request.readyState === 4 && request.status == 200) { //status
             let data = JSON.parse(request.response);
 
             inputUsd.value = inputRub.value / data.usd;
