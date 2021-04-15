816bcec15de29d34b758136b664915e44cf4a0b8
Alex <BoSacha20@gmail.com>
2021-04-15 17:20:51 +0300

    reactPtr06_55

--- petrReact/ch06/React_step_2/src/components/app-header/app-header.js
+++ petrReact/ch06/React_step_2/src/components/app-header/app-header.js
@@ -5,7 +5,8 @@ import './app-header.css'
 const AppHeader = () => {
     return (
         <div className="app-header d-flex">
-            <h1>Ivan Petrychenko</h1>
+            <h2>React_step_2 - применяем стили к нашему приложению от BoAlex</h2>
+            <h1>                                      </h1>
             <h2>5 записей, из них понравилось 0</h2>
         </div>
     )
