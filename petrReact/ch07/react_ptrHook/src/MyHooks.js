cbd164158f6dcb1045dfa126f84d49607a4d5f2e
BoAlexGit <Bosacha20@gmail.com>
2021-06-27 18:18:08 +0300

    reactPtr07_68

--- petrReact/ch07/react_ptrHook/src/MyHooks.js
+++ petrReact/ch07/react_ptrHook/src/MyHooks.js
@@ -6,14 +6,27 @@ import './App.css';
 // создадим простой hook
 function MyHooks () {
     // создаём  массив переменных
-    const [count, setCount] = useState(0) //может быть что угодно
+    const [count, setCount] = useState(0); //может быть что угодно
+    const [data, refreshData] = useState([{name:'Alex', sex: 'male'}]);
         return(
             <div>
                 <h3>Простой пример с хуками</h3>
                 <p>Вы кликнули {count} раз </p>
-                <button onClick={() => setCount(count + 1)}> Кликни меня </button>
+                <button
+                    onClick={() => setCount(count + 1)}>
+                    Кликни меня
+                </button>
+                {data.map(item => {
+                    return (// key={item.id} обязательно поставить ключ на внешний элемент
+                       <div key={item.id} ak2021 >Name: {item.name}, sex: {item.sex}</div>
+                    );
+                })}
+                <button
+                onClick={() => refreshData(data =>([...data, {name: 'Katerina', sex: 'girl'}]))}>
+                   Добавить данные
+                </button>
             </div>
-        )
+        );
 }
 
 export default MyHooks ;
